13d542cbbc7c53f0916dd40cbd7f2ee824c175c1
Nagendra Sharma <nagendra.sharma@cybersoft.net>
2020-06-03 12:27:18 +0530

    removed server code as we are using heroku

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  DB_URI: 'mongodb+srv://testuser:testUser@meetupcluster-vuwbv.mongodb.net/test?retryWrites=true&w=majority',
-  SESSION_SECRET: 'ddsrerrrresddredfr',
-  JWT_SECRET: 'ddsrerrrresddredfr'
-}
- 
- 
- //
--- server/controllers/auth.js
+++
@@ -1,4 +0,0 @@
-
-const passport = require('passport')
-
-exports.onlyAuthUser = passport.authenticate('jwt', {session: false})
